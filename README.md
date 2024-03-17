f2d308a97965d82c2635961db164c332b88b913d
Michael Peter <michaelhpet@gmail.com>
2024-03-17 23:07:15 +0100

    feat: add auth middleware

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ POST /books/author/
 PUT /books/author/
 
 NB. Whatever the response for each of this endpoints, is up to you to decide
+
+## Authentication
+
+1. Get the authentication username and password from the header.
+2. Free up the body to be able to carry payload for other method types.
+3. Add more endpoints for books [GET, POST, PUT, PATCH, DELETE]. (These endpoint don't to do anything. but must return a response)
+4. Add more endpoints for authors [GET, POST, PUT, PATCH, DELETE]. (These endpoint don't to do anything. but must return a response)
--- app.js
+++ app.js
@@ -3,41 +3,68 @@ const http = require("http");
 const server = http.createServer();
 
 server.on("request", (req, res) => {
-  if (req.url === "/books") books(req, res);
-  else if (req.url === "/books/authors") bookAuthors(req, res);
-  res.end();
+  authenticate(req, res, (req, res) => {
+    if (req.url === "/books") books(req, res);
+    else if (req.url === "/books/authors") bookAuthors(req, res);
+    res.end();
+  });
 });
 
 server.listen(8900, "localhost");
 
 /**
  * Book controller
- * @param {Request} req
- * @param {Response} res
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
  */
 function books(req, res) {
   let data = "Nothing to do with book";
   if (req.method === "GET") data = "Getting book";
+  if (req.method === "POST") data = "Creating book";
   if (req.method === "PUT") data = "Updating book";
+  if (req.method === "PATCH") data = "Updating book partially";
   if (req.method === "DELETE") data = "Deleting book";
   res.setHeader("Content-Type", "text/plain");
   res.setHeader("Content-Length", data.length);
   res.write(data);
-  res.end;
+  res.end();
 }
 
 /**
  * BookAuthor controller
- * @param {Request} req
- * @param {Response} res
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
  */
 function bookAuthors(req, res) {
   let data = "Nothing to do with author";
   if (req.method === "GET") data = "Getting author";
   if (req.method === "POST") data = "Creating author";
   if (req.method === "PUT") data = "Updating author";
+  if (req.method === "PATCH") data = "Updating author partially";
+  if (req.method === "DELETE") data = "Deleting author";
   res.setHeader("Content-Type", "text/plain");
   res.setHeader("Content-Length", data.length);
   res.write(data);
-  res.end;
+  res.end();
+}
+
+/**
+ * Auth middleware
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ * @param {http.RequestListener} next
+ */
+function authenticate(req, res, next) {
+  const username = req.headers["username"];
+  const password = req.headers["password"];
+  if (username && password) {
+    req.user = { username, password };
+    next(req, res);
+  } else {
+    const data = "Authentication required";
+    res.setHeader("Content-Type", "text/plain");
+    res.setHeader("Content-Length", data.length);
+    res.write(data);
+    res.end();
+  }
 }
